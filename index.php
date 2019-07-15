2672564300bfef2ff6200a2739de0493ccb72903
Andreas <andreas.panggabean@ccamatil.com>
2019-07-15 14:02:11 +0700

    Add Date

--- index.php
+++ index.php
@@ -14,14 +14,15 @@ $stmt = sqlsrv_query( $conn, $tsql);
 <a href="add.php">Add New User</a><br/><br/>
     <table width='80%' border=1>
     <tr>
-        <th>Name</th> <th>Email</th> <th>Job</th>
+        <th>Name</th> <th>Email</th> <th>Job</th> <th>Date</th>
     </tr>
     <?php  
     while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {        
         echo "<tr>";
         echo "<td>".$row['Name']."</td>";
         echo "<td>".$row['Email']."</td>";
-        echo "<td>".$row['Job']."</td>";    
+        echo "<td>".$row['Job']."</td>";   
+		echo "<td>".$row['CreatedDate']."</td>";   		
     }
     ?>
     </table>
