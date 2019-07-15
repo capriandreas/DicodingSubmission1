daf40371730b87f2c55c635d1097c7779dfe5bed
Andreas <andreas.panggabean@ccamatil.com>
2019-07-15 13:16:20 +0700

    Fix Config

--- config.php
+++ config.php
@@ -14,4 +14,10 @@
                              "CharacterSet"=>"UTF-8");
 
     $conn = sqlsrv_connect( $serverName, $connectionInfo);
+	
+	if($conn){
+        echo "Koneksi Berhasil";
+    }else{
+        echo "koneksi gagal";
+    }
 ?>
